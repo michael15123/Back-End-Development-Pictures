461c0092674281a83b6a794a841ad53c8668bab4
Michael <blank@gmail.com>
2023-09-11 22:03:26 +0900

    add changes

--- backend/routes.py
+++ backend/routes.py
@@ -7,18 +7,12 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
 data: list = json.load(open(json_url))
 
-######################################################################
-# RETURN HEALTH OF THE APP
-######################################################################
 
 
 @app.route("/health")
 def health():
     return jsonify(dict(status="OK")), 200
 
-######################################################################
-# COUNT THE NUMBER OF PICTURES
-######################################################################
 
 
 @app.route("/count")
@@ -30,16 +24,11 @@ def count():
     return {"message": "Internal server error"}, 500
 
 
-######################################################################
-# GET ALL PICTURES
-######################################################################
+
 @app.route("/picture", methods=["GET"])
 def get_pictures():
     return jsonify(data), 200
 
-######################################################################
-# GET A PICTURE
-######################################################################
 
 
 @app.route("/picture/<int:id>", methods=["GET"])
@@ -52,9 +41,7 @@ def get_picture_by_id(id):
     return {"message": "Picture not found"}, 404
 
 
-######################################################################
-# CREATE A PICTURE
-######################################################################
+
 @app.route("/picture", methods=["POST"])
 def create_picture():
     new_picture = request.json
@@ -68,9 +55,6 @@ def create_picture():
     
     return {"id": new_picture['id']}, 201
 
-######################################################################
-# UPDATE A PICTURE
-######################################################################
 
 
 @app.route("/picture/<int:id>", methods=["PUT"])
@@ -87,9 +71,6 @@ def update_picture(id):
     return {"message": "picture not found"}, 404
 
 
-######################################################################
-# DELETE A PICTURE
-######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
     
